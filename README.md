99a98387a0ba20c124ea1b0de15a9fde2d5f3da7
darrenwong <darrenwongf@gmail.com>
2022-02-11 08:02:55 +0800

    add vis for data

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ENVS5112_rechargeable_battery
 research project for ENVS5112
+
+## Show Cases
+
+### HKUST collection points
+
+<p align="center">
+  <img width="712pix" src="images/hkust_collection_points.png">
+</p>
+
+### Hong Kong collection points
+<p align="center">
+  <img width="712pix" src="images/hongkong_collection_points.png">
+</p>
--- main.py
+++ main.py
@@ -2,33 +2,32 @@ from flask import Flask, render_template, Response
 import folium
 import requests
 import json
+import csv
 
 app = Flask(__name__)
-
-
-iconSpiderman = folium.features.CustomIcon('./images/spiderman.png', icon_size=(100,100))
-iconHulk = folium.features.CustomIcon('./images/hulk.png', icon_size=(100,100))
-iconWolverine = folium.features.CustomIcon('./images/wolverine.png', icon_size=(100,100))
-
-#create superhero popup descriptions
-popupSpiderman = "<strong>Spiderman</strong><br>Realname: Peter Parker<br>City of birth: Forest Hills, Queens, New York, USA"
-popupHulk = "<strong>Hulk</strong><br>Realname: Bruce Banner<br>City of birth: Dayton, Ohio, USA"
-popupWolverine = "<strong>Wolverine</strong><br>Realname: James Howlett (Logan)<br>City of birth: Cold Lake, Alberta, Canada"
 
 @app.route("/")
 def base():
     # this is base map
     map = folium.Map(
         location=[22.3122029,114.1667126],
-        zoom_start=15
+        zoom_start=13
     )
     # map.add_child(folium.ClickForMarker())
-    #create superhero markers and add them to map object
-    folium.Marker([22.3122029,114.1667126], tooltip="Spiderman", popup=popupSpiderman, icon=iconSpiderman).add_to(map)
-    folium.Marker([22.3087399,114.1776188], tooltip="Hulk", popup=popupHulk, icon=iconHulk).add_to(map)
-    folium.Marker([22.3051009, 114.173681], tooltip="Wolverine", popup=popupWolverine, icon=iconWolverine).add_to(map)
 
-
+    with open('./data/rechareable_batteries.csv') as f:
+        f_csv = csv.reader(f)
+        index = 0
+        for row in f_csv:
+            lat = row[9]
+            lon = row[10]
+            # still not good way to repeat
+            iconRecycle = folium.features.CustomIcon('./images/recycle-bin.png', icon_size=(50,50))
+            folium.Marker([lat,lon], tooltip=row[4],  icon=iconRecycle).add_to(map)  
+            index = index + 1
+            # limit show 500
+            if (index>500):
+                break
     return map._repr_html_()
 
 
